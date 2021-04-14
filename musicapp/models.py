05bfd13b7cbb6a8548473e04171e405a500a2d15
TellapuramSainath <sainath.tella@gmail.com>
2021-04-14 17:09:33 +0530

    connection_of_model_songs

--- musicapp/models.py
+++ musicapp/models.py
@@ -2,13 +2,12 @@ from django.db import models
  
 # Create your models here.
  
-class Songs(models.Model):
- 
+class Songs(models.Model): 
     album_name = models.CharField(max_length=100)
     song_name = models.CharField(max_length=100)
     image_link = models.CharField(max_length=400)
     def __str__(self):
         return self.album_name
-    class Meta:
+    class meta:
         verbose_name = 'Album'
         Verbose_name_plural = "Albums"
