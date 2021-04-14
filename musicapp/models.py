376b688bde708efb4d857723ed6894fc89ce7350
thisizram <vsrraoku@gmail.com>
2021-04-14 17:59:42 +0530

    added git ignoew and updated app in settings and added db in models

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Songs(models.Model):
+ 
+    album_name = models.CharField(max_length=100)
+    song_name = models.CharField(max_length=100)
+    image_link = models.CharField(max_length=400)
+    def __str__(self):
+        return self.album_name
+    class Meta:
+        verbose_name = 'Album'
+        Verbose_name_plural = "Albums
