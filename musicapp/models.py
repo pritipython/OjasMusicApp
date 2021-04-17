8f14fab9f55d4e44829d80ea2ac9ca48e04ddae6
thisizram <vsrraoku@gmail.com>
2021-04-17 18:10:29 +0530

    use this --> update has error

--- musicapp/models.py
+++ musicapp/models.py
@@ -8,13 +8,16 @@ class Songs(models.Model):
     song_name = models.CharField(max_length=100)
     album_image_link = models.FileField(upload_to='./Album_Images')
     upload_file = models.FileField(upload_to='./Songs')
-    
+    year = models.IntegerField()
+    singer= models.CharField(max_length=200)
+
     def __str__(self):
         return self.album_name
     class Meta:
         verbose_name = 'Album'
         verbose_name_plural = "Albums"
 
-
-        #Hello u there
+#FAVORITES - USER DATA LINK
+#USER1 -> USER FOREIGN KEY AND SONGS (BUTTON) 
+#Hello u there
         
