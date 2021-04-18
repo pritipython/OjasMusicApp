69e59c86a2bd7f5670a1f64003390fb9f2b85428
thisizram <vsrraoku@gmail.com>
2021-04-19 01:52:51 +0530

    fixed playing song

--- OjasMusic/urls.py
+++ OjasMusic/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from musicapp import views as mv
 from songapp import views as sv
 
@@ -33,3 +35,4 @@ urlpatterns = [
 
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- songapp/models.py
+++ songapp/models.py
@@ -6,8 +6,8 @@ class Songs(models.Model):
  
     album_name = models.CharField(max_length=100)
     song_name = models.CharField(max_length=100)
-    album_image_link = models.FileField(upload_to='./Album_Images')
-    upload_file = models.FileField(upload_to='./Songs')
+    album_image_link = models.FileField(upload_to='./Album_Images/')
+    upload_file = models.FileField(upload_to='./Songs/')
     year = models.IntegerField()
     singer= models.CharField(max_length=200)
 
