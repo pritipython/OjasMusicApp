ca8ef2a519c7829e4cfc92945a96be491396ac72
yasaswi.arumalla <yasaswi.arumalla@ojas-it.com>
2021-04-19 17:58:32 +0530

    changed admin models login

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import GeethUsers
+from .models import GeethUsers, Songs
 # Register your models here.
 
 admin.site.register(GeethUsers)
+admin.site.register(Songs)
+
 
 
