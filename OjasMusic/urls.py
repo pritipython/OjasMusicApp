256387b8464a5865c39ad7e13aa3652ba32815b8
thisizram <vsrraoku@gmail.com>
2021-04-21 12:43:47 +0530

    minor

--- OjasMusic/urls.py
+++ OjasMusic/urls.py
@@ -32,7 +32,6 @@ urlpatterns = [
     path('list_songs_admin',sv.list_songs_admin),
     path('update/<int:id>',sv.update_songs),
     path('delete/<int:id>',sv.delete_songs),
-    path('allsongs/',sv.all_songs),
     path('album_view_songs/<slug:title>',sv.album_view),
     path('album_view/',sv.album_view),
 
