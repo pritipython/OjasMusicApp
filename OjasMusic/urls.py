eb14e2046988a4472f238558a0d34d4a71a6af82
TellapuramSainath <sainath.tella@gmail.com>
2021-04-26 21:31:37 +0530

    final_sainath_code

---
+++ OjasMusic/urls.py
@@ -0,0 +1,57 @@
+"""OjasMusic URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import include, url
+from django.urls import path
+from django.conf.urls.static import static
+from musicapp import views as mv
+from songapp import views as sv
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("userdashboard/", mv.userdashboard, name="userdashboard"),
+    path("admindashboard/", mv.admindashboard, name="admindashboard"),
+    path('login/', mv.login_request, name='login'),
+    path('signup/', mv.signup_request, name='signup'),
+    path('logout/', mv.logout_request, name='logout'),
+    path('settings/', mv.accountSettings, name='settings'),
+    path("",sv.index,name = "index"),
+    path('addsong',sv.add_songs,name ="addsong"),
+    path('list_songs',sv.list_songs_user,name="list_songs"),
+    path('list_songs_admin',sv.list_songs_admin),
+    path('delete/<int:id>',sv.delete_songs),
+    path('settings/', mv.accountSettings, name='settings'),
+    path('token' , mv.token_send , name="token_send"),
+    path('success' , mv.success , name='success'),
+    path('verify/<auth_token>' , mv.verify , name="verify"),
+    path('error' , mv.error , name="error"),   
+    path('password_reset/<int:id>' , mv.password_reset , name="password_reset"),
+    path('password_reset_email' , mv.password_reset_email , name="password_reset_email"),
+    path('password_send' , mv.password_send , name="password_send"),
+    path('password_confirm' , mv.password_confirm , name="password_confirm"),
+    path('album_view_songs/<slug:title>',sv.album_view_songs),
+    path('album_view/',sv.album_view),
+    path('myfavourite/',sv.favourite,name='myfavourite'),
+    path('addfavourite/<int:id>',sv.add_favourite,name='addfavourite'),
+    path('myfavourite/delete/<int:id>',sv.delete_fav_songs,name='deletefav')
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
---
+++ songapp/views.py
@@ -0,0 +1,106 @@
+from django.shortcuts import render,redirect
+from .forms import AddSongForm
+from .models import Songs,Favourite
+from django.core.paginator import Paginator
+
+# Create your views here.
+from .models import Songs
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from musicapp.decorators import unauthenticated_user, allowed_users
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def list_songs_admin(request):
+    list_all_songs = Songs.objects.all()
+    context={
+        'song_list':list_all_songs
+    }
+    return render(request,'list_songs_admin.html',context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['users'])
+def list_songs_user(request):
+    list_songs = Songs.objects.all()
+    p = Paginator(list_songs, 6)
+    page_number = request.GET.get('page')
+    try:
+        list_all_songs = p.get_page(page_number)
+    except PageNotAnInteger:
+        list_all_songs = p.page(1)
+    except EmptyPage:
+        list_all_songs = p.page(p.num_pages())
+    context={
+        'song_list':list_all_songs
+    }
+    return render(request,'list_songs_user.html',context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def add_songs(request):
+    form = AddSongForm()
+    if request.method=="POST":
+        form = AddSongForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("/list_songs_admin")
+    context={'addsong':form}
+    return render(request,'addsong.html',context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def delete_songs(request,id):
+    delete_song = Songs.objects.get(pk=id)
+    delete_song.delete()
+    return redirect('/list_songs_admin')
+    
+def index(request):
+    return render(request,'index.html')
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['users'])
+def album_view_songs(request,title):
+    album_songs = Songs.objects.filter(Q(album_name__iexact=title))
+    context={
+        'album_view':album_songs
+    }
+        
+    return render(request,'album_view_songs.html',context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['users'])
+def album_view(request):
+    albums = (Songs.objects.values('album_name').distinct())
+    list_albums = []
+    dict_albums = {}
+    for item in albums:
+        list_albums.append(item['album_name'])
+    for item in list_albums:
+        qs = Songs.objects.raw('select * from songapp_Songs where album_name = %s LIMIT 1',[item])[0]
+        if qs:
+            dict_albums[qs.album_name] = qs.album_image_link.url
+    print(dict_albums)
+    context = {
+        'album':dict_albums
+    }
+    return render(request, 'album_view.html',context)
+
+def favourite(request):
+    myfav_songs = Songs.objects.filter(favourite__user=request.user, favourite__is_fav=True).distinct()
+    context = {
+        'myfav_songs':myfav_songs
+    }
+    return render(request,'list_myfavourite.html',context)
+
+def add_favourite(request,id):
+    song_add = Songs.objects.get(pk=id)
+    add_fav = Favourite.objects.create(user=request.user,song=song_add,is_fav=True)
+    print(add_fav)
+    return redirect('userdashboard')
+
+def delete_fav_songs(request,id):
+    del_fav = Favourite.objects.filter(user=request.user,song__id=id)
+    del_fav.delete()
+    return redirect('myfavourite')
